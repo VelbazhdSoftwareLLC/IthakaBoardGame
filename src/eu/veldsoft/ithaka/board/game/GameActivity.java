3e41306a1e7b83d1e22103c9a6e1eadb5cd66746
Todor Balabanov <todor.balabanov@gmail.com>
2016-02-05 14:40:45 +0200

    About and help can be called from the game screen options menu.

--- src/eu/veldsoft/ithaka/board/game/GameActivity.java
+++ src/eu/veldsoft/ithaka/board/game/GameActivity.java
@@ -1,8 +1,10 @@
 package eu.veldsoft.ithaka.board.game;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class GameActivity extends Activity {
@@ -11,5 +13,32 @@ public class GameActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.game_option_menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.new_game:
+			break;
+		case R.id.help:
+			startActivity(new Intent(GameActivity.this, HelpActivity.class));
+			break;
+		case R.id.about:
+			startActivity(new Intent(GameActivity.this, AboutActivity.class));
+			break;
+		}
+		return true;
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
 	}
 }
--- src/eu/veldsoft/ithaka/board/game/HelpActivity.java
+++ src/eu/veldsoft/ithaka/board/game/HelpActivity.java
@@ -2,8 +2,6 @@ package eu.veldsoft.ithaka.board.game;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class HelpActivity extends Activity {
 
