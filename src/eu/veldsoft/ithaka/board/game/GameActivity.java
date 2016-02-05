d0ea88db15887ca0a83f9e2d444fd31082d71b63
Todor Balabanov <todor.balabanov@gmail.com>
2016-02-05 14:11:22 +0200

    Help and about screens were added.

---
+++ src/eu/veldsoft/ithaka/board/game/GameActivity.java
@@ -0,0 +1,15 @@
+package eu.veldsoft.ithaka.board.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class GameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game);
+	}
+}
