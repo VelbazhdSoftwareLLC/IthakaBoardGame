b2e9c16fd9d816595a2d29c486b3746a72b11f11
Todor Balabanov <todor.balabanov@gmail.com>
2016-02-07 18:50:57 +0200

    Model objects are almost full implemented.

--- src/eu/veldsoft/ithaka/board/game/Board.java
+++ src/eu/veldsoft/ithaka/board/game/Board.java
@@ -1,15 +1,23 @@
 package eu.veldsoft.ithaka.board.game;
 
+import java.util.Vector;
+
 class Board {
 	static final int ROWS = 4;
 	static final int COLS = 4;
 	static final int WIN_LINE_LENGTH = 3;
 
-	private Piece pieces[][] = new Piece[COLS][ROWS];
+	private Piece pieces[][] = { { Piece.BLUE, Piece.BLUE, Piece.EMPTY, Piece.FUCHSIA, Piece.FUCHSIA },
+			{ Piece.BLUE, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.FUCHSIA },
+			{ Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY },
+			{ Piece.ORANGE, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.GREEN },
+			{ Piece.ORANGE, Piece.ORANGE, Piece.EMPTY, Piece.GREEN, Piece.GREEN }, };
 
 	private int turn = 0;
 
 	private boolean gameOver = false;
+
+	private Vector<Move> moves = new Vector<Move>();
 
 	private boolean hasHorizontalLine(int i, int j) {
 		Piece current = pieces[i][j];
@@ -93,6 +101,137 @@ class Board {
 		return true;
 	}
 
+	private boolean hasEmptyPath(Move move) {
+		/*
+		 * Move to itself is not possible.
+		 */
+		if (move.startX == move.endX && move.startY == move.endY) {
+			return false;
+		}
+
+		int xStep = move.endX - move.startX;
+		int yStep = move.endY - move.startY;
+
+		/*
+		 * Move can be orthogonal or diagonal.
+		 */
+		if (xStep != 0 && yStep != 0 && Math.abs(xStep) != Math.abs(yStep)) {
+			return false;
+		}
+
+		if (xStep != 0) {
+			xStep /= Math.abs(xStep);
+		}
+		if (yStep != 0) {
+			yStep /= Math.abs(yStep);
+		}
+
+		/*
+		 * Full path should be only empty cells.
+		 */
+		for (int i = move.startX + xStep, j = move.startY + yStep; i <= move.endX
+				&& j <= move.endY; i += xStep, j += yStep) {
+			if (pieces[i][j] != Piece.EMPTY) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public int getTurn() {
+		return turn;
+	}
+
+	public boolean isGameOver() {
+		return gameOver;
+	}
+
+	public Piece[][] getPieces() {
+		return pieces;
+	}
+
+	public void reset() {
+		turn = 0;
+		gameOver = false;
+		moves = new Vector<Move>();
+
+		pieces = new Piece[][] { { Piece.BLUE, Piece.BLUE, Piece.EMPTY, Piece.FUCHSIA, Piece.FUCHSIA },
+				{ Piece.BLUE, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.FUCHSIA },
+				{ Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY },
+				{ Piece.ORANGE, Piece.EMPTY, Piece.EMPTY, Piece.EMPTY, Piece.GREEN },
+				{ Piece.ORANGE, Piece.ORANGE, Piece.EMPTY, Piece.GREEN, Piece.GREEN }, };
+	}
+
+	public void next() {
+		turn++;
+	}
+
+	public boolean isValid(Move move) {
+		/*
+		 * Last moved piece can not be moved again.
+		 */
+		if (moves.size() > 0 && move.startX == moves.lastElement().endX && move.startY == moves.lastElement().endY) {
+			return false;
+		}
+
+		/*
+		 * Check for neighbors with the same color. Observe array boundaries.
+		 */
+		int count = 0;
+		Piece piece = pieces[move.startX][move.startY];
+		for (int i = move.startX - 1; i < move.startX + 1; i++) {
+			if (i < 0 || i >= COLS) {
+				continue;
+			}
+
+			for (int j = move.startY - 1; j < move.startY + 1; j++) {
+				if (j < 0 || j >= ROWS) {
+					continue;
+				}
+
+				if (piece == pieces[i][j]) {
+					count++;
+				}
+			}
+		}
+
+		if (count <= 1) {
+			return false;
+		}
+
+		/*
+		 * Check for empty cells between start and end.
+		 */
+		if (hasEmptyPath(move) == false) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public boolean hasThirdMoveRepetition() {
+		int count = 0;
+
+		/*
+		 * Last move will be compared with moves before that.
+		 */
+		Move lastMove = moves.lastElement();
+		for (int i = moves.size() - 1; i >= 0; i -= 2) {
+			if (lastMove.equals(moves.elementAt(i)) == false) {
+				break;
+			}
+
+			count++;
+		}
+
+		if (count >= 3) {
+			return true;
+		}
+
+		return false;
+	}
+
 	public boolean hasWinner() {
 		for (int i = 0; i < pieces.length; i++) {
 			for (int j = 0; j < pieces[i].length; j++) {
@@ -101,19 +240,15 @@ class Board {
 				}
 
 				if (hasHorizontalLine(i, j) == true) {
-					gameOver = true;
 					return true;
 				}
 				if (hasVerticalLine(i, j) == true) {
-					gameOver = true;
 					return true;
 				}
 				if (hasFirstDiagonalLine(i, j) == true) {
-					gameOver = true;
 					return true;
 				}
 				if (hasSecondDiagonalLine(i, j) == true) {
-					gameOver = true;
 					return true;
 				}
 			}
---
+++ src/eu/veldsoft/ithaka/board/game/Move.java
@@ -0,0 +1,55 @@
+package eu.veldsoft.ithaka.board.game;
+
+class Move {
+	int startX;
+	int startY;
+	int endX;
+	int endY;
+
+	public Move(int startX, int startY, int endX, int endY) {
+		super();
+		this.startX = startX;
+		this.startY = startY;
+		this.endX = endX;
+		this.endY = endY;
+	}
+
+	public Move(Move move) {
+		super();
+		this.startX = move.startX;
+		this.startY = move.startY;
+		this.endX = move.endX;
+		this.endY = move.endY;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + endX;
+		result = prime * result + endY;
+		result = prime * result + startX;
+		result = prime * result + startY;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Move other = (Move) obj;
+		if (endX != other.endX)
+			return false;
+		if (endY != other.endY)
+			return false;
+		if (startX != other.startX)
+			return false;
+		if (startY != other.startY)
+			return false;
+		return true;
+	}
+}
